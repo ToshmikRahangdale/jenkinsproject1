20127d599feb3763cb6a2be940c9045db0c3ad46
ToshmikRahangdale <147581797+ToshmikRahangdale@users.noreply.github.com>
2023-11-01 13:30:19 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello{  
     public static void main(String[] args){  
-    System.out.println("This is java app \n by using jenkins");  
+    System.out.println("This is java app \n by using jenkins");
+          System.out.println("This is java app demo");
     }  
     }  
